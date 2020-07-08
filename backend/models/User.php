a2e0470ac5836574985d219d7982638c85e05a81
brant <brant88u@gmail.com>
2020-07-08 20:42:28 +0000

    Use Gii to create nesscessary files, comment,user,post

---
+++ backend/models/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $email
+ * @property string|null $profile
+ *
+ * @property Post[] $posts
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'email'], 'required'],
+            [['profile'], 'string'],
+            [['username', 'password', 'email'], 'string', 'max' => 128],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'username' => Yii::t('app', 'Username'),
+            'password' => Yii::t('app', 'Password'),
+            'email' => Yii::t('app', 'Email'),
+            'profile' => Yii::t('app', 'Profile'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Posts]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPosts()
+    {
+        return $this->hasMany(Post::className(), ['author_id' => 'id']);
+    }
+}
--- readme.md
+++ readme.md
@@ -1,7 +1,37 @@
+###create a new repository on the command line
+```
+	echo "# yiiblog" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git remote add origin git@github.com:brant8/yiiblog.git
+	git push -u origin master
+```
+其他命令可能需要使用sudo命令，使用push反而不能使用sudo命令。
+
+###push an existing repository from the command line
+```
+	git remote add origin git@github.com:brant8/yiiblog.git
+	git push -u origin master
+```
+[参考CSDN](https://blog.csdn.net/u012526120/article/details/49401871)
+
+
 ### 设置GIT连接到Github:
 	1.根据网络教程设置SSH密钥。
 	2.遇到permission denied问题后，查看本目录权限，用户组权限chown。
 	3.推送命令：
-		`git addd .`
-		`git commit -m "message"`
-		`git commit origin master`
+``
+		git addd .
+		git commit -m "message"
+		git commit origin master
+``
+
+###1.遇见的问题
+>>>Unable to write the file '/var/www/html/yiiblog/backend/models/User.php'.
+
+文件夹controllers,models,views默认权限为drwxr-xr-x。
+
+
+
+
